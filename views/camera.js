fdef6aaf87c1d17fa31669ab9619ec931778cbf8
Marco Guerra <marco.guerra@stud.unifi.it>
2021-05-08 11:49:16 +0200

    Camera download

--- views/camera.js
+++ views/camera.js
@@ -1,11 +1,6 @@
 var video = document.querySelector("#videoElement");
 var canvas = document.getElementById("canvasCamera");
 
-	function download() {
-   	var dt = canvas.toDataURL('image/jpeg');
-   	this.href = dt;
-	};
-
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true })
         .then(function (stream) {
@@ -21,20 +16,16 @@ var canvas = document.getElementById("canvasCamera");
     var video = document.getElementById("videoElement");
     button.onclick = function(event) {
         var ctx = canvas.getContext('2d');
+        ctx.filter = 'grayscale(1)';
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-        var downloadFile = document.getElementById('download');
-		downloadFile.addEventListener('click', download, false);
-
-
-        if(video.classList.contains("grayscale")){
-             canvas.classList.add('grayscale');
-             canvas.classList.remove('saturated');
-        }
-        if(video.classList.contains("saturated")){
-             canvas.classList.add('saturated');
-             canvas.classList.remove('grayscale');
-         }         
+        var download = document.createElement('a');
+		download.innerHTML = 'Download the image';
+		download.addEventListener('click', function(ev) {
+  		download.href = canvas.toDataURL();
+  		download.download = 'img.png';
+		}, false);   
+		document.body.appendChild(download); 
 
     };
 
