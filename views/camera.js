7f37e9e98a6b314a118a24aa676903f32c97ad1e
Marco Guerra <marco.guerra@stud.unifi.it>
2021-05-08 11:28:34 +0200

    Filters on canvas

--- views/camera.js
+++ views/camera.js
@@ -1,4 +1,10 @@
 var video = document.querySelector("#videoElement");
+var canvas = document.getElementById("canvasCamera");
+
+	function download() {
+   	var dt = canvas.toDataURL('image/jpeg');
+   	this.href = dt;
+	};
 
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true })
@@ -12,9 +18,24 @@ var video = document.querySelector("#videoElement");
 
     document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById("snap");
-    var canvas = document.getElementById("canvasCamera");
+    var video = document.getElementById("videoElement");
     button.onclick = function(event) {
         var ctx = canvas.getContext('2d');
-		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        var downloadFile = document.getElementById('download');
+		downloadFile.addEventListener('click', download, false);
+
+
+        if(video.classList.contains("grayscale")){
+             canvas.classList.add('grayscale');
+             canvas.classList.remove('saturated');
+        }
+        if(video.classList.contains("saturated")){
+             canvas.classList.add('saturated');
+             canvas.classList.remove('grayscale');
+         }         
+
     };
+
 });
