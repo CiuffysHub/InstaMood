5f193e709839b322f5eea3dbf273ce9f743e1ce7
Marco Guerra <marco.guerra@stud.unifi.it>
2021-05-13 13:49:14 +0200

    Upload instagram api

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/recognition', function(req, res, next) {
-  /*var path = require('path');
+  /*
   res.sendFile(path.resolve('./public/recognition.html'));*/
   res.render('recognition', { title: 'Express' });
 });
 
 
 router.get('/story', function(req, res, next) {
-    require('/public/javascripts/image-feed.js');
-  res.render('story', { title: 'Express' });
+	var path = require('path');
+    require(path.resolve('./public/javascripts/image-feed.js'));
+  	res.render('story', { title: 'Express' });
 });
 
 module.exports = router;
