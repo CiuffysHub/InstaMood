1daae7d3723a6beb0d5945f158894989e6695b17
Marco Guerra <marco.guerra@stud.unifi.it>
2021-05-18 21:56:34 +0200

    No More Express

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const http = require('http')
+const fs = require('fs')
+const url = require('url');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
+const story = require(path.resolve('./javascripts/image-feed-simulatelogin.js'))
 
-var app = express();
+http.createServer(function (req,res){
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+				var request = url.parse(req.url, true);
+  				var action = request.pathname;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.locals.basedir = __dirname
+                if(req.url === '/index.html') {
+                fs.readFile('index.html',function(err,data){
+                res.writeHead(200,{'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+              	});
+                }
+                if(req.url === '/story') {
+                story.post();
+                }
+                else if(req.url === '/stylesheets/style.css') {
+                fs.readFile('stylesheets/style.css',function(err,data){
+                res.writeHead(200,{"Content-Type": "text/css"});
+                res.write(data);
+                res.end();
+                });
+                }
+                else if(action.substr(0,13) == '/javascripts/') {
+                fs.readFile('./'+action,function(err,data){
+                res.writeHead(200,{"Content-Type": "text/javascript"});
+                res.write(data);
+                res.end();
+                });
+                }
+                if (action.substr(0,8) == '/images/') {
+			    var img = fs.readFileSync('./'+action);
+			    res.writeHead(200, {'Content-Type': 'image/jpeg' });
+			    res.end(img, 'binary');
+  				}	
 
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+            }).listen(3000);
