bfb14f37320016449552437549eb8e7bbe2f0010
1cipher <alessio.bonacchi@stud.unifi.it>
2021-05-28 13:00:45 +0200

    initial login setup

---
+++ login.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>InstaMood Login</title>
+
+		<script src="https://use.fontawesome.com/20bd40aa93.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="stylesheets/style_login.css">
+		<link rel="shortcut icon" href="#">
+
+	<body>
+		<div class="container h-100">
+			<div class="d-flex justify-content-center h-100">
+				<div class="user_card">
+					<div class="d-flex justify-content-center">
+						<div class="brand_logo_container">
+							<img class="brand_logo" src="images/logo.png" alt="InstaMood Logo with the two developer showing their moods in the foreground "></img>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<script
+			  src="https://code.jquery.com/jquery-3.6.0.min.js"
+			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+			  crossorigin="anonymous"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+
+	</body>
+</html> 
