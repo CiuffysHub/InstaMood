43dc150a93a7f475e28d144b93454b20675010e2
Marco Guerra <marco.guerra@stud.unifi.it>
2021-08-04 14:43:34 +0200

    update

--- OnePageInstaMood/php/login_page.php
+++ OnePageInstaMood/php/login_page.php
@@ -30,7 +30,6 @@
 						</div>
 					</div>
 					<div class="d-flex justify-content-center form_container">
-						<div id='wrong_credentials'></div>
 						<form>
 							<div class="input-group mb-3">
 								<div class="input-group-append">
@@ -49,12 +48,16 @@
 									<input type="checkbox" class="custom-control-input" id="customControlInline">
 									<label class="custom-control-label" for="customControlInline">Remember me</label>
 								</div>
+
 							</div>
+								<div id='wrong_credentials'></div>
 								<div class="d-flex justify-content-center mt-3 login_container">
 					 	<button type="button" name="button" id="login" class="btn login_btn">Login</button>
 					   </div>
 						</form>
+						
 					</div>
+
 			
 					<div class="mt-4">
 						<div class="d-flex justify-content-center links">
@@ -107,3 +110,4 @@
 
 	</body>
 </html>
+
