85ba01b79d99ba94f0db6ff69dfd184985b0251d
Alessio Bonacchi <alessio.bonacchi@stud.unifi.it>
2021-08-30 18:00:21 +0200

    color

--- OnePageInstaMood/index.php
+++ OnePageInstaMood/index.php
@@ -505,7 +505,7 @@ if(!isset($_SESSION['userlogin']))
       slidesToShow: 3,
       slidesToScroll: 3,
       prevArrow:'',
-      nextArrow:'<div class="row d-flex justify-content-center flex-nowrap"><div><button type="button" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path></svg>Mostra Altre</button></div></div>',
+      nextArrow:'<div class="row d-flex justify-content-center flex-nowrap"><div><button type="button" class="btn btn-primary" style="background-color:#ffb6c0; border-color:#ffb6c0;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path></svg>Mostra Altre</button></div></div>',
         responsive: [
     {
       breakpoint: 768,
