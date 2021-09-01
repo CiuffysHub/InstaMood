62b690ec3409cabf5f16e4af535a4523ab17b57f
Alessio Bonacchi <alessio.bonacchi@stud.unifi.it>
2021-09-01 17:33:17 +0200

    fix

--- OnePageInstaMood/index.php
+++ OnePageInstaMood/index.php
@@ -510,9 +510,11 @@ if(!isset($_SESSION['userlogin']))
     {
       breakpoint: 768,
       settings: {
+      infinite: true,
       slidesToShow: 1,
       centerMode: false, /* set centerMode to false to show complete slide instead of 3 */
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      nextArrow:'<div class="row d-flex justify-content-center flex-nowrap"><div><button type="button" class="btn btn-primary" style="background-color:#ffb6c0; border-color:#ffb6c0;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path></svg>Mostra Altre</button></div></div>'
       }
     }
    ]    });
